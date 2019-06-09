ac98e63519193989881c1f64f0aa8c8be7c09311
Biniyam Asnake <binicfo@gmail.com>
2019-06-09 16:41:39 -0700

    First commit.

---
+++ config.py
@@ -0,0 +1,16 @@
+# @author           Biniyam Asnake
+# @date             July 2018
+#
+# @description      config.py contains sensitive data used throughout various scrips.
+
+
+#---------------------------------------------------------------------------------------------------------------
+
+# Google Books API key
+key='&key=AIzaSyAsj14BdEN97MLaK9JLe8tzuC5aCxGI2KY'
+
+# mySql database access credentials
+port = '3306'
+database = 'care_center'
+user = 'root'
+password = 'root'
