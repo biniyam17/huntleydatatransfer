70faae3531a00a86fc3830409fc981e28a2a3fa0
Biniyam Asnake <binicfo@gmail.com>
2019-06-09 19:45:50 -0700

    new select dept query.

--- scripts/sql.py
+++ scripts/sql.py
@@ -1,6 +1,7 @@
 dept_insert_query = """INSERT INTO school_departments (school_id,department_code)
             VALUES (1,%s)"""
 dept_check_query = """SELECT COUNT(1) FROM school_departments WHERE school_id = 1 AND code = %s"""
+dept_select_query = """SELECT school_department_id FROM school_departments WHERE school_id = 1 AND code = %s"""
 
 professor_insert_query = """INSERT INTO professors (school_id,last_name)
             VALUES (1,%s)"""
